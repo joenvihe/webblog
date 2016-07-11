424782b82d0e6ce815c964593d9236371759aacc
joenvihe <enrique@vicenteh.com>
2016-07-11 14:51:40 -0500

    Se adiciono el post en view y se modifico post_list.html 2

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,7 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
+    # Se adiciona el posts
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # Se envia la variable posts
     return render(request, 'blog/post_list.html', {'posts': posts})
